fd8cb78c1348f32844dc2c79d5d3118ff8bae87d
Pramit Biswas <rijub64@gmail.com>
2023-01-16 11:32:29 +0530

    Update scaling.m

--- dip/assignment4/scaling.m
+++ dip/assignment4/scaling.m
@@ -1,4 +1,4 @@
 img  = imread('cameraman.tif');
-S = imresize(img, 0.5);
+S = imresize(img, [150 100]);
 subplot(1,2,1),imshow(img),title("Original");
 subplot(1,2,2),imshow(S),title("Scale image");
